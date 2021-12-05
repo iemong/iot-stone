f47ddd7aa6e4142f544295f34e61acf3c619b93b
iemong <kiyoshidainagon@gmail.com>
2021-12-05 18:49:09 +0900

    経過時間を計算する機能を追加

---
+++ obniz/libs/Timer.ts
@@ -0,0 +1,52 @@
+import StrictEventEmitter from "strict-event-emitter-types";
+import { EventEmitter } from "events";
+import dayjs from "dayjs";
+
+type Events = {
+  updateTime: (time: number) => void;
+};
+
+type MyEmitter = StrictEventEmitter<EventEmitter, Events>;
+
+const INTERVAL = 500;
+
+export class Timer extends (EventEmitter as { new (): MyEmitter }) {
+  private _startTime: number;
+  private _tempElapsedTime: number;
+  private _intervalTime: number;
+  private _elapsedTime: number;
+  private _timerId: NodeJS.Timer | null;
+
+  constructor(elapsedTime: number) {
+    super();
+    this._elapsedTime = elapsedTime;
+    this._startTime = 0;
+    this._tempElapsedTime = 0;
+    this._intervalTime = 0;
+    this._timerId = null;
+  }
+
+  start() {
+    if (this._timerId) return;
+    this._startTime = dayjs().valueOf();
+    this._intervalTime = this._startTime;
+
+    this._timerId = setInterval(() => {
+      this.addElapsedTime();
+    }, INTERVAL);
+  }
+
+  addElapsedTime() {
+    const currentTime = dayjs().valueOf();
+    this._tempElapsedTime = currentTime - this._intervalTime;
+    this._elapsedTime += this._tempElapsedTime;
+    this.emit("updateTime", this._elapsedTime);
+    this._intervalTime = currentTime;
+  }
+
+  stop() {
+    if (!this._timerId) return;
+    clearInterval(this._timerId);
+    this._timerId = null;
+  }
+}
--- obniz/main.ts
+++ obniz/main.ts
@@ -1,27 +1,67 @@
 import Obniz from "obniz";
 import env from "dotenv";
 import { db } from "./libs/firebase";
+import { Timer } from "./libs/Timer";
 
 env.config();
 
 const threshold = 2.0;
 let isPressure = false;
 let prevIsPressure = false;
-const obniz = new Obniz(process.env["OBNIZ_ID"] || "");
-
 const ref = db.ref("state");
 const flagRef = ref.child("isPressuring");
+const elapsedTimeRef = ref.child("elapsedTime");
 
-obniz.onconnect = async () => {
-  obniz.display?.print("watching iot stone");
-  // 5V
-  obniz.io0?.output(true);
-  // Analog Input
-  obniz.ad1?.start((voltage) => {
-    isPressure = voltage < threshold;
-    if (isPressure === prevIsPressure) flagRef.set(isPressure);
-    prevIsPressure = isPressure;
+const fetchCurrentTime = () => {
+  return new Promise<number>((resolve, reject) => {
+    elapsedTimeRef.on(
+      "value",
+      (snapshot) => {
+        const time = snapshot.val() as number;
+        resolve(time);
+      },
+      (err) => {
+        console.error(err);
+        reject(err);
+      }
+    );
   });
-  // GND
-  obniz.io2?.output(false);
 };
+
+const main = async () => {
+  const elapsedTime = await fetchCurrentTime();
+
+  const timer = new Timer(elapsedTime);
+
+  timer.on("updateTime", (time: number) => {
+    elapsedTimeRef.set(time);
+    console.info("elapsedTime:", time);
+  });
+
+  const obniz = new Obniz(process.env["OBNIZ_ID"] || "");
+
+  obniz.onconnect = async () => {
+    obniz.display?.print("watching iot stone");
+    // 5V
+    obniz.io0?.output(true);
+    // Analog Input
+    obniz.ad1?.start((voltage) => {
+      isPressure = voltage < threshold;
+
+      if (isPressure !== prevIsPressure) {
+        flagRef.set(isPressure);
+
+        if (isPressure) {
+          timer.start();
+        } else {
+          timer.stop();
+        }
+      }
+      prevIsPressure = isPressure;
+    });
+    // GND
+    obniz.io2?.output(false);
+  };
+};
+
+main();
